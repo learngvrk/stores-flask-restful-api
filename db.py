f0a426a73881a7e736da808de92ed864d2a4f1e7
= Ranjith Gonugunta <= ranjithpals@gmail.com>
2020-02-16 10:39:19 -0500

    "Added the Foreign Key constraint=ON within a try-catch block, for other db's other than sqlite"

--- db.py
+++ db.py
@@ -4,10 +4,12 @@ from sqlalchemy import event
 
 db = SQLAlchemy()
 
-
-@event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA foreign_keys=ON")
-    cursor.close()
+try:
+    @event.listens_for(Engine, "connect")
+    def set_sqlite_pragma(dbapi_connection, connection_record):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
+except:
+    pass
 
