364b30939078465b53a981e84afb097582de9343
= Ranjith Gonugunta <= ranjithpals@gmail.com>
2020-02-16 17:46:57 -0500

    "Add Foreign Key constraint for sqlite"

--- db.py
+++ db.py
@@ -1,3 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
 
 db = SQLAlchemy()
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    try:
+        cursor.execute("PRAGMA foreign_keys=ON")
+    except Exception:
+        pass
+    cursor.close()
