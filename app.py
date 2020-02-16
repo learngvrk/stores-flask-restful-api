dcd14622cc35b78bf6b64674a02629f338470a57
= Ranjith Gonugunta <= ranjithpals@gmail.com>
2020-02-16 12:34:53 -0500

    "Removing the sqlite database url"

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from resources.store import Store, StoreList
 
 # Create the Flask App
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')  #, 'sqlite:///data.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "jsn"
 
