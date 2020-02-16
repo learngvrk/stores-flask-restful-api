34befc3d2e45412a2a3aa1115a36d242d74bb0d4
= Ranjith Gonugunta <= ranjithpals@gmail.com>
2020-02-16 12:03:05 -0500

    "Check for the app.config DB connection before adding the Foreign Key constraint"

--- db.py
+++ db.py
@@ -1,15 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.engine import Engine
 from sqlalchemy import event
+from app import app
 
 db = SQLAlchemy()
 
-try:
-    @event.listens_for(Engine, "connect")
-    def set_sqlite_pragma(dbapi_connection, connection_record):
-        cursor = dbapi_connection.cursor()
-        cursor.execute("PRAGMA foreign_keys=ON")
-        cursor.close()
-except:
-    pass
-
+if app.config['SQLALCHEMY_DATABASE_URI'].contains('postgres'):
+    try:
+        @event.listens_for(Engine, "connect")
+        def set_sqlite_pragma(dbapi_connection, connection_record):
+            cursor = dbapi_connection.cursor()
+            cursor.execute("PRAGMA foreign_keys=ON")
+            cursor.close()
+    except Exception:
+        pass
